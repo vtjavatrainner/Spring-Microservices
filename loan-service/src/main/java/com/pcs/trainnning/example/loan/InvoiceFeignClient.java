b3b9a507ff6d8de19827db759958325037b9f974
vtjavatrainner <vtpcsjavatrainner@gmail.com>
2025-05-29 08:42:51 +0530

    Added eureka server code

--- loan-service/src/main/java/com/pcs/trainnning/example/loan/InvoiceFeignClient.java
+++ loan-service/src/main/java/com/pcs/trainnning/example/loan/InvoiceFeignClient.java
@@ -4,9 +4,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient("INVOICE-SERVICE")
+@FeignClient("INVOICE-SERVICE")//http://192.168.1.8:56771/
 public interface InvoiceFeignClient {
 
+    //http://192.168.1.8:56771/invoice
     @RequestMapping(method = RequestMethod.GET, value = "/invoice")
     String getInvoicesForLoan();
 }
--- loan-service/src/main/java/com/pcs/trainnning/example/loan/LoanController.java
+++ loan-service/src/main/java/com/pcs/trainnning/example/loan/LoanController.java
@@ -10,8 +10,31 @@ public class LoanController {
     @Autowired
     InvoiceFeignClient invoiceFeignClient;
 
+    @GetMapping("/loan")
+    public String getAllLoans(){
+        System.out.println("Received request for all loans");
+        return "all loans";
+    }
+
+    //@GetMapping("/loan/{loanId}/invoice")
+    /*
+        loanid:
+        loanBuyer:
+        invoices:{
+            {
+            invid:1,
+            ...
+            },
+            {
+            invId2;
+            }
+        }
+     */
     @GetMapping("/loan/invoice")
     public String getInvoiceForLoan(){
+        System.out.println("Received request for loan and its invoices");
         return invoiceFeignClient.getInvoicesForLoan();
     }
+
+
 }
